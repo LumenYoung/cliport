ffcdcce84081e6f768c424b4b138650d0d6ba032
Lumen Yang <lumen.yang@lumeny.io>
2023-10-20 14:47:40 +0200

    add script for getting demonstrations

---
+++ get_and_rundemonstrations.sh
@@ -0,0 +1,86 @@
+# python3 cliport/demos.py n=10 \
+#                         task=assembling-kits-seq-seen-colors \
+#                         mode=test
+# python3 cliport/demos.py n=10 \
+#                         task=assembling-kits-seq-unseen-colors \
+#                         mode=test
+# python3 cliport/demos.py n=10 \
+#                         task=assembling-kits-seq-full-colors \
+#                         mode=test
+# python3 cliport/demos.py n=10 \
+#                         task=put-block-in-bowl-full \
+#                         mode=test
+# python3 cliport/demos.py n=10 \
+#                         task=assembling-kits-seq-full \
+#                         mode=test
+# python3 cliport/demos.py n=10 \
+#                         task=towers-of-hanoi-seq-full \
+#                         mode=test
+
+
+python3 cliport/eval.py eval_task=assembling-kits-seq-seen-colors \
+                       model_task=multi-language-conditioned \
+                       agent=cliport \
+                       mode=test \
+                       n_demos=10 \
+                       train_demos=1000 \
+                       exp_folder=cliport_quickstart \
+                       checkpoint_type=test_best \
+                       update_results=True \
+                       disp=False
+
+python3 cliport/eval.py eval_task=assembling-kits-seq-unseen-colors \
+                       model_task=multi-language-conditioned \
+                       agent=cliport \
+                       mode=test \
+                       n_demos=10 \
+                       train_demos=1000 \
+                       exp_folder=cliport_quickstart \
+                       checkpoint_type=test_best \
+                       update_results=True \
+                       disp=False
+
+python3 cliport/eval.py eval_task=assembling-kits-seq-full-colors \
+                       model_task=multi-language-conditioned  \
+                       agent=cliport \
+                       agent=cliport \
+                       mode=test \
+                       n_demos=10 \
+                       train_demos=1000 \
+                       exp_folder=cliport_quickstart \
+                       checkpoint_type=test_best \
+                       update_results=True \
+                       disp=False
+
+python3 cliport/eval.py eval_task=put-block-in-bowl-full \
+                       model_task=multi-language-conditioned \
+                       agent=cliport \
+                       mode=test \
+                       n_demos=10 \
+                       train_demos=1000 \
+                       exp_folder=cliport_quickstart \
+                       checkpoint_type=test_best \
+                       update_results=True \
+                       disp=False
+
+python3 cliport/eval.py eval_task=assembling-kits-seq-full \
+                       model_task=multi-language-conditioned \
+                       agent=cliport \
+                       mode=test \
+                       n_demos=10 \
+                       train_demos=1000 \
+                       exp_folder=cliport_quickstart \
+                       checkpoint_type=test_best \
+                       update_results=True \
+                       disp=False
+
+python3 cliport/eval.py eval_task= towers-of-hanoi-seq-full \
+                       model_task=multi-language-conditioned \
+                       agent=cliport \
+                       mode=test \
+                       n_demos=10 \
+                       train_demos=1000 \
+                       exp_folder=cliport_quickstart \
+                       checkpoint_type=test_best \
+                       update_results=True \
+                       disp=False
