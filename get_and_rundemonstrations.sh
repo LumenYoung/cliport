ffc86e402ad6818fdc4aa5bfa48bb97b9caa3c64
Lumen Yang <lumen.yang@lumeny.io>
2023-10-27 23:09:59 +0200

    evaluation script

--- get_and_rundemonstrations.sh
+++ get_and_rundemonstrations.sh
@@ -134,13 +134,52 @@ function run_cliport() {
                          disp=False
 }
 
+function run_cliport_controled() {
+  local eval_task=$1
+
+  python3 cliport/eval.py eval_task=${eval_task} \
+                         model_task=multi-language-conditioned \
+                         agent=cliport \
+                         mode=test \
+                         n_demos=1 \
+                         train_demos=1000 \
+                         exp_folder=cliport_controled_exp \
+                         checkpoint_type=test_best \
+                         update_results=True \
+                         disp=False
+}
 
 # Define an array of task names
 # task_names=("block-insertion" "palletizing-boxes" "sweeping-piles" "put-block-in-bowl-unseen-colors")
-task_names=("palletizing-boxes" "block-insertion" "sweeping-piles" "put-block-in-bowl-unseen-colors")
+# task_names=("palletizing-boxes" "block-insertion" "sweeping-piles" "put-block-in-bowl-unseen-colors")
+
+# done testing, failing because it can't associate the color with the corresponding word.
+# task_names=("assembling-kits-seq-unseen-colors")
+
+# done testing, feedback on color can improve picking but placing is completely non-sense
+# task_names=("block-insertion")
+
+# Works fine without feedback
+# task_names=("put-block-in-bowl-seen-colors")
+
+# Some color indication works fine
+# task_names=("sweeping-piles")
+
+task_names=("separating-piles-full")
+
+# hard to feedback
+# task_names=("put-block-in-bowl-unseen-colors")
+
+# stack block pyramid
+task_names=("stack-block-pyramid-seq-seen-colors")
+
+task_names=("towers-of-hanoi-seq-full")
+
+task_names=("palletizing-boxes")
 
 # Loop over the array
 for task in "${task_names[@]}"
 do
-  run_cliport $task
+  # run_cliport $task
+  run_cliport_controled $task
 done
